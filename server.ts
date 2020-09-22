a6375c6e6f9f901f48acaef8102110e97f512ad9
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-09-21 22:35:59 -0700

    changed port to 5000 for server

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('*', (req: any, res: any) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
