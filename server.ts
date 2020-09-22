e4a396234f80884435cd2dfc1f102dfba0ca6a39
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-09-22 00:50:02 -0700

    added production serve for express

--- server.ts
+++ server.ts
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('*', (req: any, res: any) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log('Server is up!');
